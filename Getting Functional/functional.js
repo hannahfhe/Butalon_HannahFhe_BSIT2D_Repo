9ffc9798772f75a25a21bc27ba34610d88403d35
unknown <rjmeldogello6@gmail.com>
2025-03-21 16:08:42 +0800

    battleshipv2/getting functional

---
+++ Getting Functional/functional.js
@@ -0,0 +1,42 @@
+var dogname = "joshua";
+ var dogWeight = 18;
+ if (dogWeight > 20) {
+     console.log(dogname + "SAYS WOOF WOOF")
+ } else {
+     console.log (dogname + "says woof woof")
+ }
+ 
+ var dogname = "ian";
+ var dogWeight = 19;
+ if (dogWeight > 20) {
+     console.log(dogname + "SAYS WOOF WOOF")
+ } else {
+     console.log (dogname + "says woof woof")
+ }
+ 
+ var dogname = "shane";
+ var dogWeight = 25;
+ if (dogWeight > 20) {
+     console.log(dogname + "SAYS WOOF WoOOF")
+ } else {
+     console.log (dogname + "says woof woof")
+ }
+ 
+ var dogname = "freda";
+ var dogWeight = 25;
+ if (dogWeight > 20) {
+     console.log(dogname + "SAYS WOOF WOOF")
+ } else {
+     console.log (dogname + "says woof woof")
+ }
+ 
+ 
+ 
+ function bark(name, weight) {
+     if (weight > 20) {
+         console.log(name + "SAYS WOOF WOOF");
+     } else{
+         console.log(name + "says woof woof");
+     }
+ }
+ 
